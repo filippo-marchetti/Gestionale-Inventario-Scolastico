304f3d6de85a022f834230f8ca04ec4ff6b5b698
Vince <vettone.vincenzo@darzo.net>
2025-05-18 23:26:10 +0200

    fix inventario

--- pages/inventari/inventari.php
+++ pages/inventari/inventari.php
@@ -7,6 +7,8 @@ $db = 'inventariosdarzo';
 $user = 'root';
 $pass = '';
 
+$role = $_SESSION['role'];
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
